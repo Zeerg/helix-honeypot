900443d8785a23383d03d51577bf2aedfbad2a19
Tom <tbowyer@outlook.com>
2021-07-22 21:48:08 -0500

    moving to gzip

--- routes/openapi.py
+++ routes/openapi.py
@@ -1,16 +1,35 @@
 import os
+import io
+from flask import Blueprint, jsonify, render_template, make_response, request, send_file
+import gzip
 
-from flask import Blueprint, jsonify, render_template, make_response
-
+# Need to build this https://github.com/kubernetes/kube-openapi/blob/3c818078ee3de6569a8f02b6345ea3c4cc8b0998/pkg/handler/handler.go#L117
 
 openapi_routes = Blueprint("openapi_routes", __name__, url_prefix="/openapi")
 
 
 @openapi_routes.route("/v2", methods=["GET"])
 def open_api_route():
-
-    with open(os.path.abspath(os.path.dirname(__file__)) + '/openapi.json') as f:
-        resp = make_response(render_template('openapi.html', text=f.read()))
-        resp.headers['Etag'] = "D1EBA737938B05B5A35ECBE00E26C46D3D50AB8E00DDF4FE20D9EE61DC5DCC29E11C2239B7EDA7C148737D891423DFD9FCDB49BCFB4B9859C6E35A894090CC49"
-        resp.headers['Cache-Control'] = "no-cache, private"
-        return resp
+    # Vary Response Based on User Agent
+    user_agent = request.headers.get("User-Agent")
+    if "kube" in user_agent:
+        with open(
+            os.path.abspath(os.path.dirname(__file__)) + "/openapi.json", "rb"
+        ) as f:
+            response = make_response(
+                send_file(
+                    io.BytesIO(gzip.compress(f.read(), 5)),
+                    mimetype="application/octet-stream",
+                )
+            )
+            response.headers["Content-Encoding"] = "gzip"
+            response.add
+            return response
+    else:
+        with open(
+            os.path.abspath(os.path.dirname(__file__)) + "/openapi.json", "rb"
+        ) as f:
+            response = make_response(
+                send_file(io.BytesIO(f.read()), mimetype="text/plain")
+            )
+            return response
