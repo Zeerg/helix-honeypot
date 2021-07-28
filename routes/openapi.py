c89de2d467a746c37c4ea99758e75abb13b2c14a
Tom <tbowyer@outlook.com>
2021-07-27 20:39:12 -0500

    swagger is a protobuf to kubectl

--- routes/openapi.py
+++ routes/openapi.py
@@ -2,34 +2,24 @@ import os
 import io
 from flask import Blueprint, jsonify, render_template, make_response, request, send_file
 import gzip
-
-# Need to build this https://github.com/kubernetes/kube-openapi/blob/3c818078ee3de6569a8f02b6345ea3c4cc8b0998/pkg/handler/handler.go#L117
+# Need to build this https://github.com/kubernetes/kube-openapi/blob/master/pkg/handler/handler.go#L205
 
 openapi_routes = Blueprint("openapi_routes", __name__, url_prefix="/openapi")
 
 
 @openapi_routes.route("/v2", methods=["GET"])
 def open_api_route():
-    # Vary Response Based on User Agent
-    user_agent = request.headers.get("User-Agent")
-    if "kube" in user_agent:
-        with open(
-            os.path.abspath(os.path.dirname(__file__)) + "/openapi.json", "rb"
-        ) as f:
-            response = make_response(
-                send_file(
-                    io.BytesIO(gzip.compress(f.read(), 5)),
-                    mimetype="application/octet-stream",
-                )
-            )
-            response.headers["Content-Encoding"] = "gzip"
-            response.add
-            return response
+    # Vary Response Based on User Agent it's actually a protobuf https://sourcegraph.com/github.com/kubernetes/kube-openapi@dd2c4a2dc3ff0acdd86ca5a0b5b5782697bcee82/-/blob/pkg/handler/handler.go?L194:6
+    user_agent = request.headers.get('User-Agent')
+    if 'kube' in str(user_agent):
+        with open(os.path.abspath(os.path.dirname(__file__)) + '/openapi.json', 'rb') as f:
+                response = make_response(send_file(
+                    io.BytesIO(gzip.compress(f.read(), 1)),
+                    mimetype='application/octet-stream'
+                ))
+                response.headers['Content-Encoding'] = 'gzip'
+                return response
     else:
-        with open(
-            os.path.abspath(os.path.dirname(__file__)) + "/openapi.json", "rb"
-        ) as f:
-            response = make_response(
-                send_file(io.BytesIO(f.read()), mimetype="text/plain")
-            )
+        with open(os.path.abspath(os.path.dirname(__file__)) + '/openapi.json', 'rb') as f:
+            response = make_response(send_file(io.BytesIO(f.read())))
             return response
