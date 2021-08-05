44e0a12c0d715bd92d02f410827885f223aaa133
Tom <tbowyer@outlook.com>
2021-08-04 21:51:14 -0500

    moving to golang

---
+++ router/router.go
@@ -0,0 +1,13 @@
+package router
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+)
+
+func New() *echo.Echo {
+	e := echo.New()
+	e.Logger.SetLevel(log.DEBUG)
+	return e
+}
--- routes/openapi.py
+++ routes/openapi.py
@@ -12,11 +12,8 @@ def open_api_route():
     # Vary Response Based on User Agent it's actually a protobuf https://sourcegraph.com/github.com/kubernetes/kube-openapi@dd2c4a2dc3ff0acdd86ca5a0b5b5782697bcee82/-/blob/pkg/handler/handler.go?L194:6
     user_agent = request.headers.get('User-Agent')
     if 'kube' in str(user_agent):
-        with open(os.path.abspath(os.path.dirname(__file__)) + '/openapi.json', 'rb') as f:
-                response = make_response(send_file(
-                    io.BytesIO(gzip.compress(f.read(), 1)),
-                    mimetype='application/octet-stream'
-                ))
+        with open(os.path.abspath(os.path.dirname(__file__)) + '/openapi.txt', 'rb') as f:
+                response = make_response(send_file(io.BytesIO(gzip.compress(f.read(), 1)), mimetype='application/octet-stream', add_etags=True))
                 response.headers['Content-Encoding'] = 'gzip'
                 return response
     else:
