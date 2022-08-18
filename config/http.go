d73177d30bbb706c85a16b2b28ea8a1e023acabf
Tom <tbowyer@outlook.com>
2022-08-17 21:04:33 -0500

    updating golang

---
+++ config/http.go
@@ -0,0 +1,13 @@
+package config
+
+type HTTPConfig struct {
+	Host string
+	Port string
+}
+
+func LoadHTTPConfig() HTTPConfig {
+	return HTTPConfig{
+		Host: GetEnv("HELIX_HOST", "0.0.0.0"),
+		Port: GetEnv("HELIX_PORT", "8000"),
+	}
+}
